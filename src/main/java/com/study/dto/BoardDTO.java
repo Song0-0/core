1cb91622873c89278792c53703e1d1fd07b1df10
Song0-0 <ks08094@naver.com>
2024-01-16 15:58:31 +0900

    [궁금] boardList.jsp에 DB connection을 하면 DB연결실패가 된다.

---
+++ src/main/java/com/study/dto/BoardDTO.java
@@ -0,0 +1,103 @@
+package com.study.dto;
+
+public class BoardDTO {
+    private int id;
+    private int category;
+    private String title;
+    private String content;
+    private String regName;
+    private String regDt;
+    private String modName;
+    private String modDt;
+    private String views;
+    private String delYn;
+    private String userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRegName() {
+        return regName;
+    }
+
+    public void setRegName(String regName) {
+        this.regName = regName;
+    }
+
+    public String getRegDt() {
+        return regDt;
+    }
+
+    public void setRegDt(String regDt) {
+        this.regDt = regDt;
+    }
+
+    public String getModName() {
+        return modName;
+    }
+
+    public void setModName(String modName) {
+        this.modName = modName;
+    }
+
+    public String getModDt() {
+        return modDt;
+    }
+
+    public void setModDt(String modDt) {
+        this.modDt = modDt;
+    }
+
+    public String getViews() {
+        return views;
+    }
+
+    public void setViews(String views) {
+        this.views = views;
+    }
+
+    public String getDelYn() {
+        return delYn;
+    }
+
+    public void setDelYn(String delYn) {
+        this.delYn = delYn;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
